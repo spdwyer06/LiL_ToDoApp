62ec7c41c5490b2ca0d86111df7c9cf531e86a65
Sean Dwyer <spdwyer06@gmail.com>
2021-04-21 17:11:46 -0400

    Add click even for 'Mark As Completed' button in ToDoListItem

--- src/components/ToDoListItem.js
+++ src/components/ToDoListItem.js
@@ -1,12 +1,12 @@
 
 
-const ToDoListItem = ({ toDo, onRemovePressed }) => {
+const ToDoListItem = ({ toDo, onRemovePressed, onCompletedPressed }) => {
 
     return(
         <div className='todo-item-container'>
             <h3>{toDo.text}</h3>
             <div className='buttons-container'>
-                <button className='completed-btn'>Mark As Completed</button>
+                <button className='completed-btn' onClick={() => onCompletedPressed(toDo.text)}>Mark As Completed</button>
                 <button className='remove-btn' onClick={() => onRemovePressed(toDo.text)}>Remove</button>
             </div>
         </div>
