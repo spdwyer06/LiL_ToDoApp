2f6b13f1ca243c2835e723cb3a3a1433c54505a3
Sean Dwyer <spdwyer06@gmail.com>
2021-04-21 16:55:56 -0400

    Add connectability to redux dev tools in browser

--- src/index.js
+++ src/index.js
@@ -4,14 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/lib/integration/react';
 import { Provider } from 'react-redux';
 import { configureStore } from './store';
 
+const store = configureStore();
+const persistor = persistStore(store);
+
+
 ReactDOM.render(
-  <Provider store={configureStore()}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+  <Provider store={store}>
+    <PersistGate persistor={persistor} loading={<div>Loading...</div>}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
--- src/store.js
+++ src/store.js
@@ -19,4 +19,4 @@ const rootReducer = combineReducers(reducers);
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-export const configureStore = () => createStore(persistedReducer);
+export const configureStore = () => createStore(persistedReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
