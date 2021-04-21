65dc4f463b5105d9ad95b875fa9e0d953347dffb
Sean Dwyer <spdwyer06@gmail.com>
2021-04-21 16:46:16 -0400

    Install redux-persist package. Add redux persist to redux store.

--- src/store.js
+++ src/store.js
@@ -1,10 +1,22 @@
 import { createStore, combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
+
 import { toDos } from './components/reducers';
 
 const reducers = {
     toDos
 };
 
+const persistConfig = {
+    key: 'root',
+    storage,
+    stateReconciler: autoMergeLevel2
+};
+
 const rootReducer = combineReducers(reducers);
 
-export const configureStore = () => createStore(rootReducer);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const configureStore = () => createStore(persistedReducer);
