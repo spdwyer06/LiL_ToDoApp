c34f99087cad77b5ded53bc498e5710a8af65227
Sean Dwyer <spdwyer06@gmail.com>
2021-04-21 16:41:36 -0400

    Link redux to ToDoListItem

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,15 +1,25 @@
-import ToDoListItem from './ToDoListItem';
+import { connect } from 'react-redux';
+import { removeToDo } from './actions';
 
+import ToDoListItem from './ToDoListItem';
 import ToDoForm from './ToDoForm';
 
-const ToDoList = ({ toDos = [{text: 'Hello World'}] }) => {
+const ToDoList = ({ toDos = [{text: 'Hello World'}], onRemovePressed }) => {
     
     return(
         <div className='list-wrapper'>
             <ToDoForm />
-            {toDos.map(toDo => <ToDoListItem toDo={toDo} />)}
+            {toDos.map(toDo => <ToDoListItem toDo={toDo} onRemovePressed={onRemovePressed} />)}
         </div>
     );
 }
 
-export default ToDoList;
+const mapStateToProps = (state) => ({
+    toDos: state.toDos
+});
+
+const mapDispatchToProps = (dispatch) => ({
+    onRemovePressed: (text) => dispatch(removeToDo(text))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ToDoList);
--- src/components/ToDoListItem.js
+++ src/components/ToDoListItem.js
@@ -1,13 +1,13 @@
 
 
-const ToDoListItem = ({toDo}) => {
+const ToDoListItem = ({ toDo, onRemovePressed }) => {
 
     return(
         <div className='todo-item-container'>
             <h3>{toDo.text}</h3>
             <div className='buttons-container'>
                 <button className='completed-btn'>Mark As Completed</button>
-                <button className='remove-btn'>Remove</button>
+                <button className='remove-btn' onClick={() => onRemovePressed(toDo.text)}>Remove</button>
             </div>
         </div>
     );
